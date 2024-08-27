dc23d69798340625be106875fc10bec3afd4acfd
TgkCapture <tawongakanyenda5@gmail.com>
2024-08-27 19:14:00 +0200

    feat: configuring routes

--- cmd/schedulo/main.go
+++ cmd/schedulo/main.go
@@ -4,9 +4,10 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/TgkCapture/Schedulo/db"
 	"github.com/TgkCapture/Schedulo/api/handler"
 	"github.com/TgkCapture/Schedulo/config"
+	"github.com/TgkCapture/Schedulo/db"
+	"github.com/gorilla/mux"
 )
 
 func main() {
@@ -22,6 +23,13 @@ func main() {
 	if err != nil {
 		log.Fatalf("Could not create tables: %v\n", err)
 	}
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", handler.ScheduleHandler).Methods("GET")
+	r.HandleFunc("/schedules", handler.AddScheduleHandler).Methods("POST")
+	r.HandleFunc("/schedules", handler.GetSchedulesHandler).Methods("GET")
+    r.HandleFunc("/schedules/{id}", handler.DeleteScheduleHandler).Methods("DELETE")
 
 	http.HandleFunc("/", handler.ScheduleHandler)
 
